9169842ce0c36e446aa48f48096248d6402b576d
yashsoni6614 <user6hates9@gmail.com>
2026-01-12 23:52:38 +0530

    COMPLETED STL VIDEO AND NOTES

--- stl.cpp
+++ stl.cpp
@@ -1,15 +1,17 @@
-#include<bits/stdc++.h> //this header includes all the library 
+#include <bits/stdc++.h> //this header includes all the library
 
-
-using namespace std ;
+using namespace std;
 void explainPairs();
 void explainVectors();
 void explainLists();
-void explainDQ() ;
+void explainDQ();
 void explainStack();
 void explainQueue();
-void explainPQ ();
+void explainPQ();
 void explainSet();
+void explainUset();
+void explainMap();
+void explainExtra();
 int main(int argc, char const *argv[])
 {
     // explainPairs();
@@ -20,131 +22,231 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
+void explainPairs()
+{
+    pair<int, int> a = {5, 6}; // this is how a simple pair is declared
+    cout << a.first << "," << a.second << "\n";
 
-void explainPairs() {
-    pair<int ,int> a = {5,6};//this is how a simple pair is declared
-    cout<<a.first<<","<<a.second<<"\n";
+    pair<int, pair<int, int>> b = {5, {6, 7}};
+    cout << b.first << "," << b.second.second << "\n";
 
-    pair<int,pair<int,int>> b ={5,{6,7}};
-    cout<<b.first<<","<<b.second.second<<"\n";
-
-    pair<int,int>arr[] = {{3,4},{5,6}};
-    cout<<arr[0].second;
+    pair<int, int> arr[] = {{3, 4}, {5, 6}};
+    cout << arr[0].second;
 }
 
-void explainVectors() {
-    //vectors is the advanced version of the arrays they are used to make the things easier for us we can declare the size dynamically in the vectors 
-    vector<int>v;//this is how to declare a vector 
-    v.push_back(3);//this is how we can insert the elements in the vector 
-    v.emplace_back(2);//emplace back is actually faster than the vectors 
-    //vector of pairs
-    vector<pair<int,int>>vec;
-    vec.push_back({3,4});//this is how we insert the pair usingthe push back 
-    vec.emplace_back(3,4);//this is same as above but in the emplace back this gonna understand by itseld 
-    vector<int>v1(5,100); //this gonna make someting like (100,100...5 times)
-    vector<int>v2(5);
-    //there is this thing for reserving the memory
-    vector<int>v3;
-    v.reserve(10);//this gonnna reserver the memory for the elements but the size will be zero only 
-    //this is how we are gonna copy a vector into the other vector 
-    vector<int>v4(v2);//here we are copying the vector v2 into the vector v3
+void explainVectors()
+{
+    // vectors is the advanced version of the arrays they are used to make the things easier for us we can declare the size dynamically in the vectors
+    vector<int> v;     // this is how to declare a vector
+    v.push_back(3);    // this is how we can insert the elements in the vector
+    v.emplace_back(2); // emplace back is actually faster than the vectors
+    // vector of pairs
+    vector<pair<int, int>> vec;
+    vec.push_back({3, 4});  // this is how we insert the pair usingthe push back
+    vec.emplace_back(3, 4); // this is same as above but in the emplace back this gonna understand by itseld
+    vector<int> v1(5, 100); // this gonna make someting like (100,100...5 times)
+    vector<int> v2(5);
+    // there is this thing for reserving the memory
+    vector<int> v3;
+    v.reserve(10); // this gonnna reserver the memory for the elements but the size will be zero only
+    // this is how we are gonna copy a vector into the other vector
+    vector<int> v4(v2); // here we are copying the vector v2 into the vector v3
 
-    //this is how we are gonna get the iterator (iterator is differennt from the pointer pointer is the memory location while the iterator is the traversal object which knows how to traverse inside a container)
+    // this is how we are gonna get the iterator (iterator is differennt from the pointer pointer is the memory location while the iterator is the traversal object which knows how to traverse inside a container)
 
-    vector<int>::iterator it = v1.begin() ;//this is gonna point to the starting of the vector
-    //traversing to the vector 
-    //iterator follows the pointer arithmetic and the v.end points to one location after the last element 
-    for(vector<int>::iterator it = v1.begin();it!=v.end();++it) {
-        cout<<*(it);//we have to dereference this thing in here in order to make the things in here 
-
+    vector<int>::iterator it = v1.begin(); // this is gonna point to the starting of the vector
+    // traversing to the vector
+    // iterator follows the pointer arithmetic and the v.end points to one location after the last element
+    for (vector<int>::iterator it = v1.begin(); it != v.end(); ++it)
+    {
+        cout << *(it); // we have to dereference this thing in here in order to make the things in here
     }
-    for(auto it = v1.begin();it!=v1.end();++it) {
-        cout<<*(it);//we have to dereference this thing in here in order to make the things in here
+    for (auto it = v1.begin(); it != v1.end(); ++it)
+    {
+        cout << *(it); // we have to dereference this thing in here in order to make the things in here
     }
-    //auto gonna deduce this thing automatically
+    // auto gonna deduce this thing automatically
 
-    //this is the for each loop which is very easy and should be used 
-    for(auto &it :v1) {
-        cout<<it<<" ";
+    // this is the for each loop which is very easy and should be used
+    for (auto &it : v1)
+    {
+        cout << it << " ";
     }
-    it = it+2;//moves two position ahead 
-    auto it  = v1.end(); //points to one location ahead of the last element have to --it in order to access the last element 
-    v1[0];//this is how to access the elemnt 
+    it = it + 2;        // moves two position ahead
+    auto it = v1.end(); // points to one location ahead of the last element have to --it in order to access the last element
+    v1[0];              // this is how to access the elemnt
 
-
-    //this is how to insert in the vector 
-    vector<int>k(2,100);//(100,100)
-    k.insert(v.begin(),300);//)(300,100,100)
-    k.insert(v.begin()+1,2,50);//(300,50,50,100,100)
-    v.size();//it gives the size 
-    v.capacity();//this one gonna give the capacity how much is reserved before hand 
+    // this is how to insert in the vector
+    vector<int> k(2, 100);          //(100,100)
+    k.insert(v.begin(), 300);       //)(300,100,100)
+    k.insert(v.begin() + 1, 2, 50); //(300,50,50,100,100)
+    v.size();                       // it gives the size
+    v.capacity();                   // this one gonna give the capacity how much is reserved before hand
 
     //(10,20)
-    k.pop_back();//this gonna remove the last element like this (10)
+    k.pop_back(); // this gonna remove the last element like this (10)
 }
 
-void explainLists () {
-    list<int> l ; 
-    //in th elist the only funciton which is new in here is that there is the push front and the emplace front fucntion which is gonna push the element in tthe fron and rest is sane like the vector 
-    l.push_front(5);//(5)
-    l.emplace_front(6);//(6,5);
-
+void explainLists()
+{
+    list<int> l;
+    // in th elist the only funciton which is new in here is that there is the push front and the emplace front fucntion which is gonna push the element in tthe fron and rest is sane like the vector
+    l.push_front(5);    //(5)
+    l.emplace_front(6); //(6,5);
 }
-void explainDQ() {
-    //they same as the list and the vector so i am gonnaa skip this thing 
-}
-
-void explainStack() {
-    //stack is the LIFO DS 
-    //push pop and top operation is there and each operation has the time complexitu of O(1) in here
-    stack<int> st ; 
-    st.push(3);//(3)
-    st.push(5);//(3,5)
-    st.push(8);//(3,5,8)
-    st.top() ; //gonna return 8
-    st.pop(); //(3,5)
-    st.empty();//returns a boolean telling if the stack is empty or not 
-    st.size();//gonna return the size of the stack 
-    stack<int> st1,st2;
-    st1.swap(st2);//this is how we are gonna swap the two stacks in here 
-
+void explainDQ()
+{
+    // they same as the list and the vector so i am gonnaa skip this thing
 }
 
-void explainQueue(){
-    //this is similar to the stack but here it follows the FIFO principle
-    queue<int>q;
+void explainStack()
+{
+    // stack is the LIFO DS
+    // push pop and top operation is there and each operation has the time complexitu of O(1) in here
+    stack<int> st;
+    st.push(3); //(3)
+    st.push(5); //(3,5)
+    st.push(8); //(3,5,8)
+    st.top();   // gonna return 8
+    st.pop();   //(3,5)
+    st.empty(); // returns a boolean telling if the stack is empty or not
+    st.size();  // gonna return the size of the stack
+    stack<int> st1, st2;
+    st1.swap(st2); // this is how we are gonna swap the two stacks in here
+}
+
+void explainQueue()
+{
+    // this is similar to the stack but here it follows the FIFO principle
+    queue<int> q;
     q.push(1);
     q.push(2);
-    q.push(3);//(1,2,3)
-    q.front();//gonna print the 1
-    q.back();//gonna print the 3 
-    q.pop();//(2,3)
-
+    q.push(3); //(1,2,3)
+    q.front(); // gonna print the 1
+    q.back();  // gonna print the 3
+    q.pop();   //(2,3)
 }
 
-void explainPQ () {
-    priority_queue<int>pq; //priority que is the implementation of the max heap in whcih the first element is always the largest element 
-    //IN THIS PRIOTIY QUEUE THE TOP ELEMENT IS THE LARGEST ELEMENT IT IS AN ADT WHICH IS IMPLEMENTED USING  THE HEAP DS 
-    //the push pop operation takes the logn time complexity while the top operation takes the o1 
+void explainPQ()
+{
+    priority_queue<int> pq; // priority que is the implementation of the max heap in whcih the first element is always the largest element
+    // IN THIS PRIOTIY QUEUE THE TOP ELEMENT IS THE LARGEST ELEMENT IT IS AN ADT WHICH IS IMPLEMENTED USING  THE HEAP DS
+    // the push pop operation takes the logn time complexity while the top operation takes the o1
     pq.push(5);
     pq.push(2);
     pq.push(8);
     pq.emplace(10);
-    pq.top();//this gonna give 10
-    
+    pq.top(); // this gonna give 10
 
-    //this is how to implement in the min heap
-    priority_queue<int,vector<int>,greater<int>> pq; //this is how we have to implement usimg the min heap 
+    // this is how to implement in the min heap
+    priority_queue<int, vector<int>, greater<int>> pq; // this is how we have to implement usimg the min heap
 
     pq.push(5);
     pq.push(2);
     pq.push(8);
     pq.emplace(10);
-    pq.top();//this gonna give 2
-    
+    pq.top(); // this gonna give 2
 }
 
+void explainSet()
+{
+    // set is the data structure in which the things that we are gonna store is gonna be stored in the sorted order and are going to be unique
+    set<int> st;
+    st.insert(3);
+    st.emplace(5);
+    st.insert(3);
+    st.insert(2);
+    st.insert(8);
+    st.insert(1);
+    // the finally sorted order will be like this (1,2,3,5,8) no repeated occurence of the values will be there
 
-void explainSet() {
+    auto it = st.find(3); // this find method is gonna return an iterator to that location and if it is not present then it is gonna point to the null
+
+    st.erase(5); // this gonna delete the 5 from the set this takes O(ln) time complexity
+
+    int cnt = st.count(1); // this gonna tell if it is present in the list or not if yes then it gonna write 1 if no then it will be 0
+
+    auto it = st.find(4);
+    st.erase(it); // when we are gonna pass the iterator then it is gonna take the constant time for the removal of this thing
+
+    // if we wanna erase from some value to some value in the set then we have to pass the iterator like this
+    //  (1,2,3,4,5)
+    auto it1 = st.find(2);
+    auto it2 = st.find(4);
+    st.erase(it1, it2); //(1,4,5);
+
+    // in order to get the first element in here
+    auto it3 = st.begin(); //
+    cout << *(it3);
+}
+
+void explainUset()
+{
+    // unoredred set is same as the ordered set the difference in here is here the element is not stored in the sorted order
+}
+
+void explainMap()
+{
+    map<int, int> m1;
+    map<int, pair<int, int>> m2;
+    map<pair<int, int>, int> m3;
+
+    // the methods to insert some element inside the map
+    m1[1] = 2;          // here 1 is the key and 2 is the value
+    m2.insert({4,{2,3}});
+    m1.emplace(2,5);
+    //map stores the element inside in the sorted key order 
+    //this is how we are gonna iterate through the map 
+    for (auto &it :m1) {
+        cout<<it.first<<","<<it.second;
+    }
+
+
+    auto k = m1.find(3);//this is gonna return the iterator if not found it is gonna return the m1.end()
+    cout<< k->second;
+
+}
+
+void explainUnorderedMap() {
+    //this is same as the map the difference is the values are not stored in here in the sorted key order 
+    //here the searching and the inserting most of the time takes O(1) while in the ordered map it takes O(log n)
+
+}
+
+bool comp (pair<int,int>p1,pair<int,int>p2) {
+    if(p1.second<p2.second) return true;
+    else if(p1.second>p2.second) return false;
+    //they are same 
+    else if(p1.first>p2.first) return true;
+    else if(p1.first<p2.first) return false;
+    else return true;
+}
+
+void explainExtra () {
+    //in this one we are gonna see some of the algo which is necessary forr us 
+    //this is how we can sort the stuff directly using the standard stl 
+    vector<int>v {1,5,7,3,2};
+    sort(v.begin(),v.end());
     
+    pair<int,int>arr[] = {{1,2},{2,1},{4,1}};
+    //here what we want in here is we wanna sort this in this way 
+    //sort according to the second element 
+    //if second element is same then sort 
+    //it according to the first element but in descending
+
+    //comp is a funtion which returns the bool which is set according to the way we want our container to be sorted
+    sort(arr,arr+3,comp);
+
+    int num = 7 ; //binary is 00000111
+    int cnt = __builtin_popcount(num);//this returns the number of set bits(1) in the binary representation of the number
+    //cnt = 3
+    
+    
+    string s= "123";
+    do {
+        cout<<s<<"\n";
+    } while(next_permutation(s.begin(),s.end()));
+    //when all the permutations ends then it returns null
+    int ar[] = {1,3,4,5,9};
+    int maxi = *(max_element(ar,ar+5));//this returns the iterator 
 }
